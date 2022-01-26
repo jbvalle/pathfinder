2e6c69b6ebfe229cf6a4844e8e34419e86380c04
John Bryan Valle <jbvalle.jv@gmail.com>
2022-01-26 06:39:44 +0100

    updated names

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int path_finder(int *seen, char *dir, int dir_num,int row, int col){
+int path_finder(int *map, char *dir, int dir_num,int row, int col){
 
     printf("\033[1;1H\033[2J");
     for(int i = 0; i < 9; i++){
@@ -8,22 +8,26 @@ int path_finder(int *seen, char *dir, int dir_num,int row, int col){
         for(int j = 0; j < 9; j++){
 
         if((i == row)&&(j ==col)){
-            if((*(seen + 9 * i + j) != 2)){
+            if((*(map + 9 * i + j) != 2)){
                 printf("\033[1;94m");
                 printf("%c ", dir[dir_num]);
-            }else printf("%d ", *(seen + 9 * i + j));
+            }else printf("%d ", *(map + 9 * i + j));
         }
-        else if(*(seen + 9 * i + j) == 1){
+        else if(*(map + 9 * i + j) == 1){
             printf("\033[1;92m");
-            printf("%d ", *(seen + 9 * i + j));
+            printf("%d ", *(map + 9 * i + j));
         }
-        else if(*(seen + 9 * i + j) == 2){
+        else if(*(map + 9 * i + j) == 2){
             printf("\033[1;91m");
-            printf("%d ", *(seen + 9 * i + j));
+            printf("%d ", *(map + 9 * i + j));
+        }
+        else if(*(map + 9 * i + j) == 3){
+            printf("\033[1;96m");
+            printf("%d ", *(map + 9 * i + j));
         }
         else{
         
-            printf("%d ", *(seen + 9 * i + j));
+            printf("%d ", *(map + 9 * i + j));
         }
 
         printf("\033[0m");
@@ -33,23 +37,23 @@ int path_finder(int *seen, char *dir, int dir_num,int row, int col){
 
     getchar();
 
-    if((*(seen + 9 * row + col) == 1)||*(seen + 9 * row + col) == 2)return 0;
+    if((*(map + 9 * row + col) == 1)||*(map + 9 * row + col) == 2)return 0;
 
     if((row < 0) || (row > 8) || (col < 0) || (col > 8))return 0;
 
-    if(*(seen + 9 * row + col) == 3){
+    if(*(map + 9 * row + col) == 3){
         printf("\nEND FOUND!");
         return 1;
     }
 
-    *(seen + 9 * row + col) = 1;
+    *(map + 9 * row + col) = 1;
 
 
     
-    return path_finder(seen, dir, 0, row-1, col) ||
-          path_finder(seen, dir, 1, row, col + 1)||
-          path_finder(seen, dir, 2, row + 1, col)||
-          path_finder(seen, dir, 3, row, col-1);
+    return path_finder(map, dir, 0, row-1, col) ||
+          path_finder(map, dir, 1, row, col + 1)||
+          path_finder(map, dir, 2, row + 1, col)||
+          path_finder(map, dir, 3, row, col-1);
 
 }
 
@@ -57,7 +61,7 @@ int path_finder(int *seen, char *dir, int dir_num,int row, int col){
 int main(void){
 
     char dir[4] = {'^', '>', 'V', '<'};
-    int seen[9][9] = {
+    int map[9][9] = {
                     
         {0,0,0,0,2,0,0,0,0},
         {0,0,0,0,2,0,2,2,0},
@@ -70,7 +74,7 @@ int main(void){
         {0,0,0,0,0,2,3,0,0},
     };
 
-    path_finder((int*)seen, dir, 0, 0, 0);
+    path_finder((int*)map, dir, 0, 0, 0);
 
 
 
